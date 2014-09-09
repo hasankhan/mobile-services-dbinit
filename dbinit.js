1eb3e4a80ccdb3b9580255ad981625f66f2f3cc0
Hasan Khan <slashdot10@gmail.com>
2014-09-09 15:54:20 -0700

    facted out the dbinit module

---
+++ dbinit.js
@@ -0,0 +1,21 @@
+﻿var path = require('path'),
+    fs = require('fs'),
+    TableBuilder = require('./tablebuilder');
+
+exports = module.exports = function (context, done) {
+    var tablesPath = context.tablesPath || path.resolve(__dirname, '../table')
+    var schemaName = context.schemaName || (context.serviceName || process.env.MS_MobileServiceName).replace(/-/g, '_');
+    var idType = context.idType || 'string';
+
+    var builder = new TableBuilder(schemaName);
+
+    var files = fs.readdirSync(tablesPath);
+    files.forEach(function (file) {
+        if (path.extname(file) !== '.json') return;
+
+        var tableName = file.substring(0, file.indexOf('.'));
+        var query = builder.getSql(tableName, idType);
+
+        context.mssql.query(query);
+    });
+};
---
+++ tablebuilder.js
@@ -0,0 +1,35 @@
+﻿var SQL_CREATE_TABLE = "IF NOT EXISTS (" +
+                            "SELECT 1 FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = '%1$s' AND TABLE_NAME = '%2$s'" +
+                       ") CREATE TABLE [%1$s].[%2$s] (%3$s)",
+    SQL_DEF_ID_STRING = "[id] NVARCHAR(255) PRIMARY KEY NONCLUSTERED DEFAULT CONVERT(nvarchar(255), NEWID())",
+    SQL_DEF_ID_INT = "[id] [int] IDENTITY(1,1) NOT NULL";
+
+var TableBuilder = (function () {
+    function TableBuilder(schemaName) {
+        this.schemaName = schemaName;
+    }
+
+    TableBuilder.prototype.getSql = function (tableName, idType) {
+        var columnDefs = [],
+            self = this;
+
+        columnDefs.push(this._getIdDefinition(idType));
+
+        var query = SQL_CREATE_TABLE.replace(/%1\$s/g, this.schemaName)
+                                     .replace(/%2\$s/g, tableName)
+                                     .replace(/%3\$s/g, columnDefs.join(','));
+
+        return query;
+    }
+
+    TableBuilder.prototype._getIdDefinition = function (type) {
+        if (type == 'number') {
+            return SQL_DEF_ID_INT;
+        }
+        return SQL_DEF_ID_STRING;
+    }
+
+    return TableBuilder
+})();
+
+exports = module.exports = TableBuilder;
