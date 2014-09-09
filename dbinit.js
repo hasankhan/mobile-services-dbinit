ee7ae6465b89fe36c55ff50a4f173d249a430545
Hasan Khan <slashdot10@gmail.com>
2014-09-09 16:40:38 -0700

    validate the table name is valid

--- dbinit.js
+++ dbinit.js
@@ -2,6 +2,8 @@
     fs = require('fs'),
     TableBuilder = require('./tablebuilder');
 
+var tableNamePattern = /^[_a-z][a-z0-9_]{0,59}$/i;
+
 exports = module.exports = function (context, done) {
     var tablesPath = context.tablesPath || path.resolve(__dirname, '../table')
     var schemaName = context.schemaName || (context.serviceName || process.env.MS_MobileServiceName).replace(/-/g, '_');
@@ -14,6 +16,11 @@ exports = module.exports = function (context, done) {
         if (path.extname(file) !== '.json') return;
 
         var tableName = file.substring(0, file.indexOf('.'));
+        // validate its a valid table name
+        if (!tableNamePattern.test(tableName)) {
+            return;
+        }
+
         var query = builder.getSql(tableName, idType);
 
         context.mssql.query(query, {
--- startup.js
+++ startup.js
@@ -1,4 +1,5 @@
 module.exports.startup = function (context, done) {
+    // calling db initializer asynchronously to avoid delaying the startup of app
     require('./dbinit')(context);
     done();
 }
